281ac9bfe539ac3bdc28631c930f877e9ef65b23
mdyousufhossain <yousafhossain3@gmail.com>
2022-05-17 22:19:28 +0600

    pinpongGame

--- VanilaJavascript/Pingpong/app.js
+++ VanilaJavascript/Pingpong/app.js
@@ -2,10 +2,30 @@ const ball = document.querySelector('#gameBoard');
 
 ball.style.border = '1px solid black'
 ball.style.borderRadius = '50%'
-ball.style.marginTop = '100px';
 
-ball.style.marginLeft = '100px';
 
-window.addEventListener('keydown', (e)=>{
-    console.log(e.keyCode);
+var    left    = 0;
+var    top     = 0;
+var    right   = 0;
+var    down    = 0;
+
+document.addEventListener('keydown', (e)=>{
+
+    if(e.keyCode === 39){
+        left++
+        ball.style.marginLeft = `${left}px`;
+        console.log(left)
+    }    else if(e.keyCode === 40){
+        down++
+        
+        ball.style.marginTop = `${down}px`;
+    }else if(e.keyCode === 38){
+        top++
+        ball.style.marginBottom = `${top}px`;
+    }
 })
+
+
+
+document.addEventListener('keydown',e => console.log(e.keyCode) )
+
