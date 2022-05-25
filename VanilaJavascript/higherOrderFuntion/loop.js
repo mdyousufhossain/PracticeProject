088fc014401f92454ac9da94b559dac62c5d008c
mdyousufhossain <yousafhossain3@gmail.com>
2022-05-25 16:11:35 +0600

    somechnage

--- VanilaJavascript/higherOrderFuntion/loop.js
+++ VanilaJavascript/higherOrderFuntion/loop.js
@@ -48,6 +48,7 @@ console.log(reformattedArray)
 // reformattedArray is now [{1: 10}, {2: 20}, {3: 30}],
 
 // kvArray is still:
+
 // [{key: 1, value: 10},
 //  {key: 2, value: 20},
 //  {key: 3, value: 30}]
