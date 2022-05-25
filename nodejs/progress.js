e502990f09bd9fa55590ace2a895b707db9e9edc
mdyousufhossain <yousafhossain3@gmail.com>
2022-05-25 21:09:20 +0600

    progress

---
+++ nodejs/progress.js
@@ -0,0 +1,9 @@
+const ProgressBar = require('progress');
+
+const bar = new ProgressBar(':bar', { total: 10 });
+const timer = setInterval(() => {
+  bar.tick();
+  if (bar.complete) {
+    clearInterval(timer);
+  }
+}, 100);
