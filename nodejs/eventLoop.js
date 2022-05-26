dc57a292f20fc63bb8b733f22dcb2bdcffe42723
mdyousufhossain <yousafhossain3@gmail.com>
2022-05-26 14:50:07 +0600

    eventLoopUp

---
+++ nodejs/eventLoop.js
@@ -0,0 +1,12 @@
+const bar = () => console.log('bar')
+
+const baz = () => console.log('baz')
+
+const foo = () => {
+  console.log('foo')
+  bar()
+  baz()
+}
+
+foo()
+
--- nodejs/progress.js
+++ nodejs/progress.js
@@ -7,3 +7,29 @@ const timer = setInterval(() => {
     clearInterval(timer);
   }
 }, 100);
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  readline.question(`What's your name?`, name => {
+    console.log(`Hi ${name}!`);
+    readline.close();
+  });
+
+const inquirer = require('inquirer');//need inquirer npm installation 
+
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your name?",
+  },
+];
+
+inquirer.prompt(questions).then(answers => {
+  console.log(`Hi ${answers.name}!`);
+});
+
