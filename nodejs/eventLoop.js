0aa8d9e9e573259d13330ce98290a7b74e67d10b
mdyousufhossain <yousafhossain3@gmail.com>
2022-05-26 20:18:23 +0600

    update

--- nodejs/eventLoop.js
+++ nodejs/eventLoop.js
@@ -1,12 +1,29 @@
+// const bar = () => console.log('bar')
+
+// const baz = () => console.log('baz')
+
+// const foo = () => {
+//   console.log('foo')
+//   bar()
+//   baz()
+// }
+
+// foo()
+
 const bar = () => console.log('bar')
 
 const baz = () => console.log('baz')
 
 const foo = () => {
   console.log('foo')
-  bar()
+  setTimeout(bar, 0)
+  new Promise((resolve, reject) =>
+    resolve('should be right after baz, before bar')
+  ).then(resolve => console.log(resolve))
   baz()
 }
 
 foo()
 
+//coper T - 50 
+// T - 20
