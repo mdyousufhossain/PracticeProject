e9a172be7cb172490de5fe609e7e273cafeee571
mdyousufhossain <yousafhossain3@gmail.com>
2022-05-30 23:35:42 +0600

    final of the day

--- ReactProject/newprops/src/App.js
+++ ReactProject/newprops/src/App.js
@@ -1,9 +1,18 @@
 import React from "react"
 import { useEffect } from "react";
+import './App.css';
+import { BiSearchAlt2 } from "react-icons/bi";
+// import SearchIcon from './search.svg';
 
-
-//process.env.REACT_APP_API_KEY
 const _API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=6e952c93`;
+
+const movie1 = {
+        "Title": "Doctor Strange",
+        "Year": "2016",
+        "imdbID": "tt1211837",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg"
+    }
 
 const App = () =>{
     const searchMovies = async(title) =>{
@@ -14,14 +23,36 @@ const App = () =>{
     }
     
     useEffect(() =>{
-        searchMovies('')
+        searchMovies('doctor strange')
         
     },[]);
 
     return (
-            <div>
+            <div className="app">
                 
-                <h1>Hello there</h1>
+                <h1>Find Movie</h1>
+                <div className="search">
+                  <input placeholder="Search Movies"
+                //   value={}
+                //onChange={()=>{}}
+                  /> 
+                    <h3 onClick={()=>{}}> 
+                        <BiSearchAlt2 />  
+                    </h3>
+                </div>
+                <div className="container">
+                    <div className="movie">
+                        <div>
+                            <p> {movie1.Year}</p>
+                        </div>
+                        <div>
+                            <img src={movie1.Poster} alt={movie1.Title} />
+                        </div>
+                        <div>
+                            <span>{movie1.Type}</span>
+                        </div>
+                    </div>
+                </div>
             </div>
     );
 }
