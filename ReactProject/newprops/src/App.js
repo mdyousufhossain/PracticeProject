9f93a90ba57467d530c0feb232be002b98c3011b
mdyousufhossain <yousafhossain3@gmail.com>
2022-05-31 14:33:09 +0600

    moviesproject

--- ReactProject/newprops/src/App.js
+++ ReactProject/newprops/src/App.js
@@ -6,39 +6,40 @@ import MovieCard from "./MovieCard";
 
 const _API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=6e952c93`;
 
-const movie1 = {
-        "Title": "Doctor Strange",
-        "Year": "2016",
-        "imdbID": "tt1211837",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg"
-    }
+// const movie1 = {
+//         "Title": "Doctor Strange",
+//         "Year": "2016",
+//         "imdbID": "tt1211837",
+//         "Type": "movie",
+//         "Poster": "https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg"
+//     }
 
 const App = () =>{
     const [movies,setMovies] = useState([]);
-
+    const [searchTerm,setSerachTerm] = useState([])
     const searchMovies = async(title) =>{
         const response = await fetch(`${_API_URL}&s=${title}`)
         const data = await response.json();
 
-        searchMovies(data.Search)
+        setMovies(data.Search)
     }
     
     useEffect(() =>{
-        searchMovies('doctor strange')
+        searchMovies('Spiderman')
         
     },[]);
 
     return (
-            <div className="app">
-                
+            <div className="app">  
                 <h1>Find Movie</h1>
+
                 <div className="search">
                   <input placeholder="Search Movies"
-                //   value={}
-                //onChange={()=>{}}
-                  /> 
-                    <h3 onClick={()=>{}}> 
+                    value={searchTerm}
+                    onChange={(e)=>{setSerachTerm(e.target.value)}}
+                  />
+
+                    <h3 onClick={()=>searchMovies(searchTerm)}> 
                         <BiSearchAlt2 />  
                     </h3>
                 </div>
@@ -46,9 +47,9 @@ const App = () =>{
                      movies?.length>0
                      ? (
                         <div className="container">
-
-                        <MovieCard movie1={movies[0]}/>
-
+                            {movies.map((movie) =>(
+                                <MovieCard movie={movie} />
+                            ))}
                         </div>
                        ):(
                        <div className="empty">
