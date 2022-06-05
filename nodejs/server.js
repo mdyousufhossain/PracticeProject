4756935a57f3c6bc2989e5cffa319ea8d740197a
mdyousufhossain <43577099+mdyousufhossain@users.noreply.github.com>
2022-06-05 20:59:17 +0600

    Update server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs =require('fs');
-
+//this will drop server
 const server = http.createServer(function(req,res){
     res.write('Hellow World!');
     res.write('Hellow World!');
