eebf61d1a7c9e9f7527626f7f7bc651e856455aa
mdyousufhossain <43577099+mdyousufhossain@users.noreply.github.com>
2022-06-09 23:35:45 +0600

    Update currency.js

--- fetchingData/currency.js
+++ fetchingData/currency.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 // gypOxrI2MlXELoKBJILO5GcmuDEE5c8f
 
 const getExchangeRate = (fromCurrency,toCurrency) =>{
+    //this api is from a channel that is provide currency information its not updated
     axios.get('http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1').then((response)=>{
         const rate =  response.data.rates;
         const euro = 1 / rate[fromCurrency];
