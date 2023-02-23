82d1b64151429df787613991ab2128017d11f9f9
mdyousufhossain <yousafhossain3@gmail.com>
2023-02-23 23:47:40 +0600

    added

--- BoxMatchingGame/index.js
+++ BoxMatchingGame/index.js
@@ -1,5 +1,11 @@
 tailwind.config = {
     theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+          },
       extend: {
         colors: {
           clifford: '#da373d',
@@ -7,3 +13,17 @@ tailwind.config = {
       }
     }
   }
+
+  const nodeBackGround = document.getElementById('parentNode');
+
+
+
+
+
+//dsfsdfsdfsd
+nodeBackGround.addEventListener('click', function(e) {
+    if(e.target.id !== 'parentNode'){
+        e.target.style.backgroundColor = "blue"
+    } 
+    
+});
