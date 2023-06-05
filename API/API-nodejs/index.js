d75de56b1645b6d55475c65fa6100abd1645af5b
mdyousufhossain <yousafhossain3@gmail.com>
2023-06-05 21:46:54 +0600

    added dynamic route

--- API/API-nodejs/index.js
+++ API/API-nodejs/index.js
@@ -1,25 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-
+const express = require("express");
+const app = express();
+const port = 3000;
 
 // app.use((res,req) => {
 //     res.res.send({"color":"Red"})
 
 // })
 
-app.get("/dogs",(res,req) => {
-    res.res.send({"type":"animal","name":"dog","attribute":"Loyality"})
-})
+app.get("/dogs", (res, req) => {
+  res.res.send({ type: "animal", name: "dog", attribute: "Loyality" });
+});
 
+app.get("/", (res, req) => {
+  res.res.send({ type: "animal", name: "Human", attribute: "humanity" });
+});
 
-app.get("/",(res,req) => {
-  res.res.send({"type":"animal","name":"Human","attribute":"humanity"})
-})
+app.get("/fun", (res, req) => {
+  res.res.send("<h1>hey hey this is just for fun </h1>");
+});
 
+app.get("/users/:user/", (res, req) => {
+  const { user } = req.req.params;
+  res.res.send(`<h1>Welcome home ${user} </h1>`);
+});
 
+app.post("/create", (res, req) => {
+  res.res.send("creating post ");
+});
+
+app.get("*", (res, req) => {
+  res.res.send("<h1> Invalid page please check your direction </h1>");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port : http://localhost:3000/`)
-})
+  console.log(`Example app listening on port : http://localhost:3000/`);
+});
