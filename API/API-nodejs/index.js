7a44d3f2680818e410be234024eb74a74c8c1eb7
mdyousufhossain <yousafhossain3@gmail.com>
2023-06-06 13:25:08 +0600

    making a CRUD APP

--- API/API-nodejs/index.js
+++ API/API-nodejs/index.js
@@ -1,63 +1,58 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
+const path = require('path')
 
-// app.use((res,req) => {
-//     res.res.send({"color":"Red"})
+app.use(express.urlencoded({ extended:true }))
+app.use(express.json())
 
-// })
+app.set('views', path.join(__dirname, "views"))
 
-app.get("/dogs", (res, req) => {
-  res.res.send({ type: "animal", name: "dog", attribute: "Loyality" });
-});
+app.set('view engine','ejs')
 
-app.get("/", (res, req) => {
-  res.res.send({ type: "animal", name: "Human", attribute: "humanity" });
-});
-
-app.get("/fun", (res, req) => {
-  res.res.send("<h1>hey hey this is just for fun </h1>");
-});
-
-app.get("/search",(req,res) => {
-  const { q } = req.query;
-  if(!q){ 
-    res.send("nothing found if nothing searched")
+const comments = [
+  {
+    username:'Bob',
+    comment: "shit this shit is funny hahahah"
+  },
+  {
+    username:'wolf',
+    comment: "fucking hell do that shit"
+  },
+  {
+    username:'siyabith',
+    comment: "iki mono ga kari "
   }
-  res.send(`<h1> Search results for : ${q}</h1>`)
+]
+
+app.get('/comments',(req,res) => {
+  res.render('comments/index', { comments })
+})
+ 
+
+app.get('/comments/new', (req, res ) => {
+    res.render('comments/new')
 })
 
-app.get("/users/:user/", (res, req) => {
-  const { user } = req.req.params;
-  res.res.send(`<h1>Welcome home ${user} </h1>`);
-});
-
-app.get("/users/:user/:post", (res, req) => {
-  const { user , post } = req.req.params;
-  res.res.send(`
-    <h1>Welcome home ${user} </h1>
-      <h2> there it is your post ${post}</h2>
-  `);
-});
-
-app.post("/create", (res, req) => {
-  res.res.send("creating post ");
-});
-
-app.get("/bad/:word",(res,req) => {
-  const { word } = req.req.params
-
-  res.res.send(`<h1> here is your bad word:</h1> <h3> ${word} </h3> `)
-
+app.post('/comments',(req,res) => {
+    const { username,comment } = req.body
+    console.log(req.body)
+    comments.push({ username,comment })
+    res.send("it working yeay ")
 })
 
+app.get("/form",(res,req) => {
+    console.log(req.req.body)
+    res.res.send("this is home page")
+})
 
-app.get("*", (res, req) => {
-  res.res.send("<h1> Invalid page please check your direction </h1>");
-});
+app.post("/form",(res,req) => {
+    const { fname , sname } =  req.req.body 
 
-
+    res.res.send(`<h1> this is ${fname} and their surename is ${sname} </h1>`)
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port : http://localhost:3000/`);
-});
+    
+    console.log("server running on http://localhost:8080/")
+})
