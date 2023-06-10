0a4700cd3414a54eb48159d7fa8657b50903aed7
mdyousufhossain <yousafhossain3@gmail.com>
2023-06-10 12:27:35 +0600

    added mongodb connected

--- TODO-APP/backend/server.js
+++ TODO-APP/backend/server.js
@@ -1,7 +1,23 @@
+const dotenv = require("dotenv").config()
 const express = require("express");
+const connectDB = require('./config/connectDB')
+
+
 
 const app = express();
 const port = 3000;
+
+connectDB()
+
+app.get("/", (req,res) => {
+  res.send("hey this is response")
+})
+
+
+
 app.listen(port, () => {
   console.log(`server started at : http://localhost:${port}/`);
 });
+
+
+// mongodb+srv://yousafhossain:<yousaf123>@tododatabase.cmkwxhd.mongodb.net/?retryWrites=true&w=majority
