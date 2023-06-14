cefce71edf442032144998a56d1b5df52e3d3106
mdyousufhossain <yousafhossain3@gmail.com>
2023-06-14 17:30:51 +0600

    input side is completed

--- TODO-APP/fontend/src/component/TaskBody/AddingTask/AddingTask.jsx
+++ TODO-APP/fontend/src/component/TaskBody/AddingTask/AddingTask.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
 const AddingTask = () => {
+  const [items, setItem] = useState({});
+  const [inputvalue, setInputValue] = useState("");
+
+
+  const handleSubmit = () => {
+    if (inputvalue.trim() !== "") {
+      setItem({ ...items, inputvalue });
+      console.log(items);
+    }
+    setInputValue("");
+  };
+
   return (
     <div className="w-full">
       <div className="md:w-4/5 mx-auto">
@@ -7,13 +21,20 @@ const AddingTask = () => {
             className="w-full p-6 rounded-md mr-4"
             type="text"
             placeholder="Add a Task"
+            value={inputvalue}
+            onChange={(e) => setInputValue(e.target.value)}
           />
-          <button className="w-28 h-12 bg-slate-400 rounded">Add task</button>
+
+          <button
+            className="w-28 h-12 bg-slate-400 rounded"
+            onClick={handleSubmit}
+          >
+            Add task
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-
-export default AddingTask
+export default AddingTask;
