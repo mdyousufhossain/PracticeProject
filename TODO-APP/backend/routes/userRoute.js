2a502d6838e753817741b68bd958506e0634f143
mdyousufhossain <yousafhossain3@gmail.com>
2023-06-29 00:10:18 +0600

    bkadas

--- TODO-APP/backend/routes/userRoute.js
+++ TODO-APP/backend/routes/userRoute.js
@@ -2,7 +2,7 @@ const  express  = require("express");
 const registerUser = require("../controllers/userController")
 
 const router = express.Router()
-
+// new user regirster route
 router.post("/register",registerUser)
 
 module.exports = router
