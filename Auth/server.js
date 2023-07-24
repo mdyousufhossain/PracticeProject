b62910a075b68b54374c9bddc54a3b7fe287a9c6
mdyousufhossain <yousafhossain3@gmail.com>
2023-07-24 12:53:30 +0600

    making new api

--- Auth/server.js
+++ Auth/server.js
@@ -5,7 +5,7 @@ const expess = require("express");
 const app = expess();
 const PORT = 8080;
 
-app.get("/dashboard", (req, res) => {
+app.get("/:user/dashboard", (req, res) => {
   res.send("this is admin panel");
 });
 
@@ -13,15 +13,32 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-function Server() {
-  https.createServer({
-    
-    key :fs.readFileSync('key.pem'),
-    cert :fs.readFileSync('cert.pem')
 
-  }).listen(PORT, () => {
-    console.log(`server started in ${PORT} `);
-  });
+app.post('/:user/dashboard',(req , res) => {
+  res.send('user Created')
+})
+
+
+
+
+
+// function Server() {
+//   https.createServer({
+    
+//     key :fs.readFileSync('key.pem'),
+//     cert :fs.readFileSync('cert.pem')
+
+//   }).listen(PORT, () => {
+//     console.log(`server started in ${PORT} `);
+//   });
+// }
+
+// Server();
+
+function fakeServer() {
+    app.listen(PORT, () => {
+        console.log("server started at", PORT)
+    })
 }
 
-Server();
+fakeServer()
