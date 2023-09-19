80a38e17db9cb6ec6bd7b5d33261f5ecd713f19f
mdyousufhossain <yousafhossain3@gmail.com>
2023-09-19 23:44:00 +0600

    updgrading khidos sidebar

--- orbit-people/components/shared/Topbar.tsx
+++ orbit-people/components/shared/Topbar.tsx
@@ -1,7 +1,10 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Topbar() {
+    const isUserLoggedIn = true;
+
     return (
         <>
             <nav className="topbar">
@@ -10,6 +13,37 @@ export default function Topbar() {
 
                     <p className="text-headings3-bold text-light-1 max-xs:hidden">Khido</p>
                 </Link>
+
+                <div className="flex items-center gap-1">
+                    <div className="block md:hidden">
+                        <SignedIn>
+                            <SignOutButton>
+                                <div className="flex cursor-pointer">
+                                    <Image
+                                    src='/assets/logout.svg'
+                                    alt="logout"
+                                    width={24}
+                                    height={24}
+                                    />
+                                </div>
+                            </SignOutButton>
+                        </SignedIn>
+                    </div>
+
+                <OrganizationSwitcher 
+                    appearance={
+                        {
+                            elements: {
+                                button: {
+                                    organizationSwitcherTrigger:
+                                    'py-2 px-4'
+                                }
+                            }
+                        }
+                    }
+                
+                />
+                </div>
             </nav>
         </>
     )
