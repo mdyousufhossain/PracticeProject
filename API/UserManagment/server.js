abc2504e70bb123e8070e224f39c70812ad42081
mdyousufhossain <yousafhossain3@gmail.com>
2023-09-20 18:00:51 +0600

    sharingan

--- API/UserManagment/server.js
+++ API/UserManagment/server.js
@@ -7,18 +7,24 @@ const app =  express()
 
 const PORT = 5050 
 
-app.listen(PORT ,() => {
-    console.log(`server started at : http://localhost:${PORT}/`
-    )
-})
 
 
 
 
 app.get('/' , (req, res ) => {
-
+    
     res.send("hey this is home")
 })
 
-console.log(app.listenerCount())
+app.get('/test' , (req, res ) => {
+    //route for testing 
+    res.send("hey this is test",)
+})
+
+app.listen(PORT ,() => {
+    console.log(`server started at : http://localhost:${PORT}/`,
+    )
+})
+
+
 
