823c0c5ed0797075795217162216152d36577e0d
mdyousufhossain <yousafhossain3@gmail.com>
2023-09-25 23:38:21 +0600

    crud system is full functional now

--- API/UserManagment/Controller/userController.js
+++ API/UserManagment/Controller/userController.js
@@ -11,6 +11,13 @@ const getAlluser = async (req, res) => {
   }
 };
 
+/*
+
+  singe user detector this one is only for who are authenticated
+  and authorized user
+  roles : author,Admin
+
+*/
 const getSingleUser = async (req, res) => {
   try {
     const { id } = req.params;
@@ -26,8 +33,55 @@ const getSingleUser = async (req, res) => {
   }
 };
 
+/*
+  
+Roles : admin only
+
+*/
+
+const getSingleUserAndDelete = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const User = await Userdb.findByIdAndDelete(id);
+
+    if (!User) return res.status(404).json({ message: `No User was found : ${id}` });
+
+    res.status(200).json(User);
+    console.log(`User Deleted ! ${id}`);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+const getSingleUserAndUpdate = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const User = await Userdb.findByIdAndUpdate(
+      { _id: id }, // verifying the id
+      req.body, // updating the body
+      {
+        new: true, // new item will be accepted
+        runValidators: true,
+      }
+    );
+
+    if (!User) return res.status(404).json({ message: `No User was found : ${id}` });
+
+    res.status(200).json(User);
+    console.log(`User Updated ${id}`);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+
+
 
 module.exports = {
   getAlluser,
-  getSingleUser 
+  getSingleUser,
+  getSingleUserAndDelete,
+  getSingleUserAndUpdate
 };
--- API/UserManagment/Route/userRoute.js
+++ API/UserManagment/Route/userRoute.js
@@ -1,16 +1,23 @@
-const  register  = require('../Controller/registerController')
-const { getAlluser , getSingleUser } = require('../Controller/userController')
-const loginHandler = require('../Controller/loginController')
-require('dotenv').config();
+const register = require("../Controller/registerController");
+const {
+  getAlluser,
+  getSingleUser,
+  getSingleUserAndDelete,
+  getSingleUserAndUpdate,
+} = require("../Controller/userController");
+const loginHandler = require("../Controller/loginController");
+require("dotenv").config();
 
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+
+router
+  .post("/register", register)
+  .get("/alluser", getAlluser)
+  .get("/:id", getSingleUser)
+  .post("/login", loginHandler)
+  .delete("/:id", getSingleUserAndDelete)
+  .put('/:id' , getSingleUserAndUpdate)
 
 
-
-router.post('/register', register)
-router.get('/alluser', getAlluser)
-router.get('/:id', getSingleUser )
-router.post('/login', loginHandler)
-
-module.exports = router
+module.exports = router;
