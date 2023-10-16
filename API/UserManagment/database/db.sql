0bdab1ad160e000a69242601204b3e96603c5e3b
mdyousufhossain <yousafhossain3@gmail.com>
2023-10-16 20:45:13 +0600

    more sql

--- API/UserManagment/database/db.sql
+++ API/UserManagment/database/db.sql
@@ -1,8 +1,17 @@
 CREATE TABLE product(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    desc VARCHAR(255),
+    product_name VARCHAR(60) NOT NULL,
+    details VARCHAR(255),
     photo VARCHAR(255),
-    stock INTEGER NOT NULL CHECK (population>0),
+    stock INTEGER NOT NULL CHECK (stock>0),
     sold INTEGER 
+);
+
+CREATE TABLE org(
+    id SERIAL PRIMARY KEY,
+    org_name VARCHAR(60) NOT NULL,
+    details VARCHAR(255),
+    photo VARCHAR(255),
+    members Array(30) CHECK (members>0),
+    createdby VARCHAR(255) 
 );
